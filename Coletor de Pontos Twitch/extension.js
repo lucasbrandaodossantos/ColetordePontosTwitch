4ede07b40d6726707c83621fc8a6df7b11a89c65
lucasbrandaodossantos <lucasbrandaodossantos@outlook.com>
2020-08-11 23:09:00 -0300

    reorganização de arquivos para uma pasta, organização melhor do projeto

--- Coletor de Pontos Twitch/extension.js
+++ Coletor de Pontos Twitch/extension.js
@@ -1,12 +1,16 @@
 console.log("Entrei na extensão!")
-const applicationInterval = setInterval(checkButton, 10000);
+//Ler a página da Twitch
+const interval = setInterval(()=>{
 
-function checkButton(){
-    console.log("CHECANDO BOTÃO...")
-    const button = document.querySelector("span.tw-button__text");
-    if (button) {
-        button.click();
-        console.log("Ponto coletado!!!!");
-    }        
-    console.log(button)
-}
+    const button = document.querySelector(".tw-button tw-button--success tw-interactive");
+    if (button){
+        button.addEventListener("click", () => {
+            console.log("ponto coletado!");
+            clearInterval(interval)
+        })
+    }       
+}, 3000)
+
+//Identificar o ícone de pontos
+
+//Relógio para acionar de 3 em 3 minutos
