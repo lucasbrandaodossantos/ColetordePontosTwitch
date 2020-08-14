3b3424bdeca639185f4109541712a2411feae2ec
lucasbrandaodossantos <lucasbrandaodossantos@outlook.com>
2020-08-14 14:30:08 -0300

    Reorganização de pastas - final

--- extension.js
+++ extension.js
@@ -1,16 +1,13 @@
 console.log("Entrei na extensão!")
 //Ler a página da Twitch
-const interval = setInterval(()=>{
+const interval = setInterval(() => {
 
     const button = document.querySelector(".tw-button tw-button--success tw-interactive");
-    if (button){
+    if (button) {
+        //Identificar o ícone de pontos
         button.addEventListener("click", () => {
             console.log("ponto coletado!");
             clearInterval(interval)
         })
-    }       
+    }
 }, 3000)
-
-//Identificar o ícone de pontos
-
-//Relógio para acionar de 3 em 3 minutos
